c6d94fdd3d9a7d7330db7e88a7a45a187d94f755
Amirprogrammer689 <beeline13062015@gmail.com>
2024-05-08 09:54:56 +0400

    period 100

--- LAB1_radio/Source.cpp
+++ LAB1_radio/Source.cpp
@@ -10,7 +10,7 @@
 #define FFT_POINTS2 ((double)FFT_POINTS * (double)FFT_POINTS)
 #define FS 1.0E+6
 
-const double F = 20000; // Frequency of the input signal
+const double F = 100000; // Frequency of the input signal
 const double DT = 1.0 / FS; // Sampling interval
 const double DF = FS / FFT_POINTS; // Frequency step
 const double Mag = 1.0; // Magnitude of the input signal
@@ -25,7 +25,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 {
 	double S[FFT_POINTS]; // Массив для отображения сигнала
 	char Txt[512];
-	sprintf_s(Txt, sizeof(Txt), "samples_period_20.txt");
+	sprintf_s(Txt, sizeof(Txt), "samples_period_100.txt");
 	memset(&ofn, 0, sizeof(OPENFILENAME));
 	ofn.lStructSize = sizeof(OPENFILENAME);
 	ofn.hwndOwner = NULL;
